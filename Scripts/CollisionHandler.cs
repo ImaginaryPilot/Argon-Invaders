25524ef41835b08bf834c5b73d064e6b027c83fa
ImaginaryPilot <107716532+ImaginaryPilot@users.noreply.github.com>
2023-12-24 21:21:26 +0100

    Launch

---
+++ Scripts/CollisionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [SerializeField] GameObject Explosion;
+    void OnTriggerEnter(Collider other)
+    {
+        DeathSequence();
+    }
+
+    void DeathSequence()
+    {
+        SendMessage("OnDeath");
+        Explosion.SetActive(true);
+        Invoke("Restart", 1f);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
